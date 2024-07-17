355ff7cc2c98a30bf8985c6e58e84f83534ad549
Himanshu Jain <112754762+Himanshuj0311@users.noreply.github.com>
2024-07-17 17:07:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ hamburger.addEventListener("click", (e)=>{
 
 //Function for Resume to open on the next tab
 function resume(){
-    window.open("https://drive.google.com/file/d/13btzQ-evuR2f0n8MfuJGIpej5SisFiMG/view","_blank")
+    window.open("https://drive.google.com/file/d/162UgpSBzTvRKM807-v3QEsbmOl2E5vSp/view","_blank")
   }
 
 // Third-party library to disply GitHub Calender
